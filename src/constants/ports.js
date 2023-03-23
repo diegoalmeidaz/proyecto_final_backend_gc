9f30e5179ad5cab5bd7e963f397cca436dcde0f1
Diego Almeida <diegoalmeidaz@gmail.com>
2023-03-23 03:31:27 -0300

    temporary fix to make the server run again

--- src/constants/ports.js
+++ src/constants/ports.js
@@ -6,6 +6,6 @@ module.exports = {
   PORT: process.env.PORT,
   SERVER_URL: process.env.SERVER_URL,
   CLIENT_URL: process.env.SERVER_URL,
-  SECRET: process.env.SECRET,
+  SECRET: "6742890",
   SECRET_KEY: process.env.SECRET_KEY,
 }
