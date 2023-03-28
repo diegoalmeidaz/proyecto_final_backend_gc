ee792b7b9afdb4d86d005cce2f5c93df48446ca3
Diego Almeida <diegoalmeidaz@gmail.com>
2023-03-28 07:31:12 -0300

    minor swagger update

--- src/routes/items.js
+++ src/routes/items.js
@@ -4,6 +4,85 @@ const { isNil } = require("lodash");
 const pool = require("../db/pool");
 const itemController = require("../controllers/itemController");
 
+
+/**
+ * @swagger
+ * tags:
+ *   name: Items
+ *   description: Operaciones relacionadas con los items.
+ */
+
+/**
+ * @swagger
+ * /items:
+ *   get:
+ *     summary: Obtiene una lista de todos los productos con filtros opcionales.
+ *     description: Devuelve una lista de todos los productos almacenados en la base de datos. Puede ser filtrado por nombre, color, tamaño, marca, diseñador independiente, precio mínimo y precio máximo.
+ *     tags:
+ *       - Items
+ *     parameters:
+ *       - in: query
+ *         name: query
+ *         description: Filtro de búsqueda opcional para el nombre del producto.
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: colors
+ *         description: Filtro de búsqueda opcional para el color del producto.
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: sizes
+ *         description: Filtro de búsqueda opcional para el tamaño del producto.
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: brands
+ *         description: Filtro de búsqueda opcional para la marca del producto.
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: independentFilter
+ *         description: Filtro de búsqueda opcional para los vestidos de diseñador independiente.
+ *         schema:
+ *           type: boolean
+ *       - in: query
+ *         name: minPrice
+ *         description: Filtro de búsqueda opcional para el precio mínimo del producto.
+ *         schema:
+ *           type: number
+ *       - in: query
+ *         name: maxPrice
+ *         description: Filtro de búsqueda opcional para el precio máximo del producto.
+ *         schema:
+ *           type: number
+ *     responses:
+ *       '200':
+ *         description: Lista de productos.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 products:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Product'
+ *                 maxPrice:
+ *                   type: number
+ *       '500':
+ *         description: Error interno del servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ */
+
+
+
 router.get('/', async (req, res) => {
   try {
     const items = await itemController.getItems(req.query);
