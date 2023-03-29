1c4e400360c38cf38e443d511993019a1591e211
Diego Almeida <diegoalmeidaz@gmail.com>
2023-03-29 04:53:49 -0300

    trying to fix secret for jwt key as i'm having trouble with deployment

--- src/constants/ports.js
+++ src/constants/ports.js
@@ -1,13 +1,12 @@
 const { config } = require('dotenv')
 config()
-const { PORT_ENV, SERVER_URL, CLIENT_URL, SECRET_ENV, SECRET_KEY } = process.env;
+const { PORT_ENV, SERVER_URL, CLIENT_URL, SECRET, SECRET_KEY } = process.env;
 
 module.exports = {
   PORT: PORT_ENV,
   SERVER_URL: SERVER_URL,
   CLIENT_URL: CLIENT_URL,
-  SECRET: SECRET_ENV,
+  SECRET: SECRET,
   SECRET_KEY: SECRET_KEY,
 }
  // comentario de prueba nueva redirigida github
- 
